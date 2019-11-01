d7074a5b9dd3a6c02b905d643aef13df24132d97
zgma-test <uditharosha+zgma@gmail.com>
2019-11-01 14:14:03 +0530

    sw

--- SampleAPI/lambda.js
+++ SampleAPI/lambda.js
@@ -1,9 +1,23 @@
+let Swagger = require('swagger-client');
 // let api = require('./api.json');
-let api = require('./skipped-api-test.json');
+let api = require('./skipped-apis-test.json');
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
 
     console.log(api);
-    
+    Swagger.http({
+        url: `https://api.sandbox.transferwise.tech/quotes`,
+        method: 'post',
+        query: {},
+        headers: { "Accept": "application/xml", "Content-Type": "application/json" },
+        body: JSON.stringify({
+            "a": 123
+        })
+    }).then((response) => {
+        // your code goes here
+    }).catch((err) => {
+        // error handling goes here
+    });
+
     callback(null, api);
 }
